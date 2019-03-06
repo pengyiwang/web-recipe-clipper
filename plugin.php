ebdfb6fdf5217cb74283f3f035ca8ac307a0a5dc
Pengyi Wang <pengyi@peachdish.com>
2019-03-06 09:10:19 +0800

    update plugin description

--- plugin.php
+++ plugin.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Plugin Name: web-recipe-clipper — CGB Gutenberg Block Plugin
- * Plugin URI: https://github.com/ahmadawais/create-guten-block/
- * Description: web-recipe-clipper — is a Gutenberg plugin created via create-guten-block.
- * Author: mrahmadawais, maedahbatool
- * Author URI: https://AhmadAwais.com/
+ * Plugin Name: web-recipe-clipper
+ * Plugin URI: https://www.leancodes.com
+ * Description: web-recipe-clipper — Sometimes we want to save a recipe that we find on the web in our own blog post, you can use this plugin to add a block to gutenberg editor, then you can fill in the web recipe url. It will generate a recipe in your editor based on the web recipe title, photo, description, ingredients and instructions, you can edit it, change it to your own version then publish it.
+ * Author: LeanCodes
+ * Author URI: https://www.leancodes.com
  * Version: 1.0.0
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
